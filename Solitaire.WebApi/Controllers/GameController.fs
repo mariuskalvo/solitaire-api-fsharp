e234195daacca7734434e0b42088aa419a70757d
Marius Kalvø <mariuskalvo30@gmail.com>
2021-08-11 19:49:30 +0200

    Get game by id in f# controller

--- Solitaire.WebApi/Controllers/GameController.fs
+++ Solitaire.WebApi/Controllers/GameController.fs
@@ -3,12 +3,19 @@
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
 open GameService
+open System
 
 [<ApiController>]
 [<Route("[controller]")>]
 type GameController (logger : ILogger<GameController>, gameService : GameService) =
     inherit ControllerBase()
 
-    [<HttpGet>]
+    [<HttpGet()>]
     member _.Get() =
         gameService.GetAllGames()
+
+    [<HttpGet("{id}")>]
+    member _.GetById(id: Guid) =
+        gameService.GetGameById(id)
+
+    
